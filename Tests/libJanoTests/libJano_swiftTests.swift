1f0c9acf168cb67c9d134f4dca6da8008623b3cb
Ezequiel (Kimi) Aceto <ezequiel.aceto@gmail.com>
2022-01-01 12:42:16 +0100

    Init Swift Package

---
+++ Tests/libJanoTests/libJano_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import libJano_swift
+
+final class libJano_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(libJano_swift().text, "Hello, World!")
+    }
+}
